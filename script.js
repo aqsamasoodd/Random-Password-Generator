3f58ed5ef3a65f17eb509c3a8694e0c811ef0d6a
Aqsa Masood <aqsamasoodd1922@gmail.com>
2025-09-29 16:59:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()-+~|}{[]<>/_+";
+
+const allCharcter = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (lenght > password.length) {
+    password += allCharcter[Math.floor(Math.random() * allCharcter.length)];
+  }
+
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  navigator.clipboard.writeText(passwordBox.value);
+}
+
